15aa5ef136a6ee5b6a52e19b7abac96d009e17de
SharatRokkam <rokkamsharath1@gmail.com>
2026-04-01 20:07:06 +0530

    started js

---
+++ javascript/basic.js
@@ -0,0 +1,55 @@
+// Dowload and install nodejs on laptop and use any online editor for mobile
+
+// html - document
+
+// Chrome, Firefox, Opera, Edge, etc..... - Browser
+// JS Engine  - programs and calculation....
+// Chrome - V8 Engine, Firefox - Spider Monkey, Edge - Chakra
+
+//What is Javascript ?
+
+// Programming Language -  7000+
+// Binary Language - 0  and 1
+
+// High Level Dynamic Programming Language
+//High Level - easy to understand for humans
+//Low Level -difficult to understand for humans but easy for computer
+
+// Javascript is a high level interpreted dynamic programming language
+// high level - easy to understand
+//interpreted - code gets converted line by line
+// dynamic and static
+// programming lang means set of instruction given to computer
+
+// *******Very Important************
+// It is also called single threaded synchronous language.
+
+
+// To see output in the console
+// console.log("First Line of code");
+// console.log("second of line code");
+// console.log("third of line code ");
+
+// language - English - alphabets, grammar - noun, pronoun, conjuction, statement
+
+
+// variables - are just a container or a box to store a value or data 
+
+// three ways to create a variable 
+// 1. var 2. let 3. const 
+
+var score = 76;
+let emailid = "tes@gmail.com"
+const name123 = "Tarun";
+
+let $myName = "Sharath";
+const my_rollno = 232;
+
+// Rules to follow 
+
+// 1. variables are case sensitive - score and Score are different 
+// 2. variables cannot start with number - 123name is not correct , name123 is correct
+// 3. You cannot use JavaScript reserved keywords (such as let, const, function, return, if, etc.) as variable names.
+// 4.  Names can contain letters (a-z, A-Z), digits (0-9), underscores (_), and dollar signs ($).
+// 5.No Spaces or Hyphens: Variable names cannot contain spaces or hyphens (-)
+
