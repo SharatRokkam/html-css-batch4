3f98e11318dccc5e8b4cc80b7393a5e682b66ddc
SharatRokkam <rokkamsharath1@gmail.com>
2026-04-07 20:07:23 +0530

    remaining array methods

--- javascript/intermediate.js
+++ javascript/intermediate.js
@@ -110,4 +110,89 @@ let age = 30;
 
 // console.log(a.concat(b));
 
-// push, pop, shift, unshift, concat, join 
+// push, pop, shift, unshift, concat, join
+
+// ******************************************************************************
+// 7th April 2026 - Tuesday
+
+// Array of object - JSON data
+
+// let users = [
+//   { name: "Kavitha", isUser: true },
+//   { name: "Asif", isUser: false },
+//   { name: "Sai Ramana", isUser: true },
+//   { name: "Meghana", isUser: false },
+//   { name: "Naresh", isUser: false },
+// ];
+// let names = users.map((user) => user.isUser);
+// console.log(names);
+
+// // console.log(users[1])
+
+// let numbers = [1, 2, 3, 4, 5];
+
+// let multiply = numbers.map(number => number * 3);
+
+// console.log(multiply);
+
+// Filter : is used to filter the records
+
+// let numbers = [2, 4, 5, 3, 7, 9, 11, 12];
+
+// let even = numbers.filter(number => number % 2 != 0)
+
+// console.log(even);
+
+// let users = [
+//   { name: "A", active: false },
+//   { name: "B", active: true },
+//   { name: "C", active: true },
+// ];
+
+// let activeUsers = users.filter((user) => user.active);
+// console.log(activeUsers.length);
+
+// Reduce  - Interviewer's favourite
+
+// let my_cart = [
+//   { item: "Shirt", price: 699 },
+//   { item: "Pants", price: 899 },
+//   { item: "Watch", price: 999 },
+//   { item: "Shoes", price: 1499 },
+// ];
+
+// // reduce
+// let total = my_cart.reduce((sum, item) => sum + item.price, 0);
+// console.log(total);
+
+// slice
+
+let items = [1, 2, 4, 5, 6];
+
+// let page1 = items.slice(0, 4);
+let page1 = items.slice(-2);
+// console.log(page1);
+
+
+
+// let users = [
+//   { id: 1, name: "A" },
+//   { id: 2, name: "B" },
+//   { id: 3, name: "C" },
+//   { id: 4, name: "D" },
+// ];
+
+// find method will be used excessively in database
+// let user = users.find((user) => user.id === 4);
+// console.log(user);
+
+// let data = [1, [2, "Sharath"], [4, 5, 3, 4]];
+
+// console.log(data.flat());
+
+// let randomNum = [1, 23, 33, 111, 22, 34, 445, 55, 22, 556];
+
+// console.log(randomNum.sort((a, b) => a - b));
+
+
+// Task : Splice, some, every, includes, findIndex, at, charAt - Minimum 5 completed
