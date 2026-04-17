4ec63cbb1127496ee16cb99bd910bd91a3b88dbb
SharatRokkam <rokkamsharath1@gmail.com>
2026-04-17 20:12:03 +0530

    fetch, api, json, promises, asyncawait

--- javascript/async.js
+++ javascript/async.js
@@ -20,18 +20,39 @@
 // three ways of async implement
 // 1. Callback function is function that takes another function as an argument
 
-//Avoid using callbacks function as it will lead you to callback hell that creates unreadibilty in the code 
+//Avoid using callbacks function as it will lead you to callback hell that creates unreadibilty in the code
 
-function fetchData(callback) {
+// function fetchData(callback) {
+//   setTimeout(() => {
+//     callback("data received");
+//   }, 1000);
+// }
+
+// function newFunc(data) {
+//   console.log(data);
+// }
+
+// fetchData(newFunc);
+
+// Promises - task
+
+const promise = new Promise((resolve, reject) => {
   setTimeout(() => {
-    callback("data received");
-  }, 1000);
+    console.log("Data Received");
+  }, 2000);
+});
+
+promise.then((data) => console.log(data)).catch((err) => console.log(err));
+
+// Async Await
+
+async function getData() {
+  try {
+    const data = await promise;
+    console.log("data received again");
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-function newFunc(data) {
-  console.log(data);
-}
-
-fetchData(newFunc);
-
-// Promises 
+getData();
---
+++ javascript/fetch/fetch.js
@@ -0,0 +1,29 @@
+const button = document.querySelector("#btn");
+const container = document.querySelector("#container");
+
+button.addEventListener("click", fetchUsers);
+
+async function fetchUsers() {
+  container.innerHTML = "loading...";
+
+  try {
+    const response = await fetch("https://dummyjson.com/users");
+    const { users } = await response.json();
+
+    container.innerHTML = "";
+
+    users.forEach((user) => {
+      const div = document.createElement("div");
+      div.innerHTML = `
+            <h1>${user.firstName} </h1>
+            <h3>${user.email}
+            <span>Age : ${user.age}</span>
+        `;
+
+      container.appendChild(div);
+    });
+  } catch (error) {
+    console.log(error);
+    container.innerHTML = "Error fetching data";
+  }
+}
