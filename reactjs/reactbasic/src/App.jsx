1bf3688d139f096589206fd81007dc2caa345d77
SharatRokkam <rokkamsharath1@gmail.com>
2026-04-23 20:15:19 +0530

    useState and vdom

--- reactjs/reactbasic/src/App.jsx
+++ reactjs/reactbasic/src/App.jsx
@@ -1,6 +1,13 @@
-// JSX
+import Counter from "./Counter";
+
+// JSX - js and
 function App() {
-  return <h1>Hello ! Everyone to ReactJS</h1>;
+  return (
+    <div>
+      {/* Counter looks like a tag but it is a way of passing components  */}
+      <Counter />
+    </div>
+  );
 }
 
 export default App;
